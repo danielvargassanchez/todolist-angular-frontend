a5f68e4cc09ce2c7c141025330b09a03d2af1810
Daniel <danielvargassanchez@hotmail.com>
2020-07-24 11:05:16 -0500

    interfaces finished

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
   {
-    path: 'login',
+    path: '',
     component: LoginComponent,
   },
   {
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  form: FormGroup;
+  showModal: boolean = false;
 
-  constructor() { }
+  constructor(private fbForm: FormBuilder) {}
 
   ngOnInit(): void {
+    this.form = this.fbForm.group({
+      title: ['', Validators.required],
+      description: ['', Validators.required],
+    });
   }
 
+  showForm() {
+    this.showModal = !this.showModal;
+  }
+
+  addTask() {}
 }
